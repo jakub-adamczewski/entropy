123acf29bb1ae985becbc07efd174d3575485c50
jakub-adamczewski <1999adamek@gmail.com>
2022-11-15 20:34:27 +0100

    entropy check

--- 1_task_english_characters_entropy.py
+++ 1_task_english_characters_entropy.py
@@ -1,6 +1,9 @@
 from util import get_entropy
 from util import get_letters_probabilities
+from scipy.stats import entropy
 
 probabilities = get_letters_probabilities(file="data/norm_wiki_en.txt")
 print(probabilities)
-print(get_entropy(probabilities.values()))
+prob_list = list(probabilities.values())
+print("My entropy:", get_entropy(prob_list))
+print("Lib entropy", entropy(prob_list, base=2))
