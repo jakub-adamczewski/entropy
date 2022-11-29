009d3d9224cd1ec48d88fd39580464d11c7a29a0
jakub-adamczewski <1999adamek@gmail.com>
2022-11-29 22:59:37 +0100

    refactor

--- task_3_1_char_conditional_entropy.py
+++ task_3_1_char_conditional_entropy.py
@@ -3,4 +3,4 @@ import entropies
 file = "data/norm_wiki_en.txt"
 level = 1
 first_level_entropy = entropies.get_conditional_entropy_chars(file=file, level=level)
-print("First level (one preceding letter) conditional entropy in English:", first_level_entropy)
+print("First level (one preceding letter) char conditional entropy in English:", first_level_entropy)
